98fdf425a36a83d25e7a4f8a50ba4b4fb2f8078d
cristalynllarenas19-oss <cristalynllarenas19@gmail.com>
2026-06-19 20:28:35 +0800

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,7 +27,7 @@ console.log("🔥 MAIN.TS LOADED");
       origin: string | undefined,
       callback: (err: Error | null, allow?: boolean) => void,
     ) => {
-      if (!origin || origins.includes(origin)) {
+      if (!origin || origins.length === 0 || origins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -36,10 +36,8 @@ console.log("🔥 MAIN.TS LOADED");
     credentials: true,
   });
 
-  const port = Number(process.env.API_PORT) || 3001;
-
+  const port = process.env.PORT || 3001;
   await app.listen(port, "0.0.0.0");
-
   console.log(`Backend running on port ${port}`);
   console.log(`madikon ketnana. magiging tambay lang din naman`);
   console.log(`pabasbas ng programming skills, li xun`);
